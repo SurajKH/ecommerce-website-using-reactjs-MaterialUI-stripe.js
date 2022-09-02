b358cf85661e60af7fd81cad4bfcb139351538ac
surajkh <90398336+SurajKH@users.noreply.github.com>
2022-09-02 18:19:45 +0530

    Update commerce.js

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,4 +1,4 @@
 import Commerce from '@chec/commerce.js';
 
-export const commerce = new Commerce('pk_44238a381731169a2883bf622bcd985c824940c14a4a4',true);
+export const commerce = new Commerce('COMMERCE-API-KEY',true);
 
